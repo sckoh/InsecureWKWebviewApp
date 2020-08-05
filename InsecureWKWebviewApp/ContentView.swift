1c1329e71ecd6cd686558b51b75bda0f60218560
sckoh <soocheng.koh@ifastfinancial.com>
2020-08-05 13:16:47 +0800

    Initial Commit

---
+++ InsecureWKWebviewApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InsecureWKWebviewApp
+//
+//  Created by soo cheng koh on 05/08/2020.
+//  Copyright © 2020 soo cheng koh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
